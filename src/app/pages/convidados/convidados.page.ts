0cd19a9fc4708bc082ebf3f160db60786bd8a03d
Mauricio Amorim <mjvamorim@gmail.com>
2019-07-09 21:23:54 -0300

    Versao 02a - ConvidadosService instalado mas nao testado

--- src/app/pages/convidados/convidados.page.ts
+++ src/app/pages/convidados/convidados.page.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { EditConvidadoPage } from '../edit-convidado/edit-convidado.page';
 import { Convidado } from '../../models/convidado';
+import { LoadingController } from '@ionic/angular';
+import {ConvidadosService} from "../../service/convidados.service"
 @Component({
   selector: 'app-convidados',
   templateUrl: './convidados.page.html',
@@ -11,12 +13,26 @@ export class ConvidadosPage implements OnInit {
   convidados: any;
   convidado: Convidado;
 
-  constructor(private router: Router) {
+  constructor(private router: Router, public api: ConvidadosService, public loadingController: LoadingController) {
       this.getConvidados();
   }
 
-  getConvidados() {
-    this.convidados = [{"id":6,"nome":"Ricardo","qtde":109},{"id":13,"nome":"Fernanda","qtde":8}];
+  async getConvidados() {
+    //this.convidados = [{"id":6,"nome":"Ricardo","qtde":109},{"id":13,"nome":"Fernanda","qtde":8}];
+
+    const loading = await this.loadingController.create({
+      message: 'Loading'
+    });
+    await loading.present();
+    await this.api.getConvidados()
+      .subscribe(res => {
+        console.log(res);
+        this.convidados = res;
+        loading.dismiss();
+      }, err => {
+        console.log(err);
+        loading.dismiss();
+      });
   }
   ngOnInit() {
   }
